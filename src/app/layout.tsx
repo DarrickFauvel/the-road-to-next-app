ec1386e9cb27b261d30a606f5221354d8449440a
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-15 16:54:01 +0000

    update home link text and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { LucideKanban } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,8 +33,9 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-bbg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-              <Link href={homePath()} className={buttonVariants({variant: "outline"})}>
-                Home
+              <Link href={homePath()} className={buttonVariants({variant: "ghost"})}>
+                <LucideKanban />
+                <h1 className="text-lg font-semibold">TicketBounty</h1>
               </Link>
           </div>
           <div>
