0ebf750ae06ea2dda5d0d2ee8020e3f0f551d25e
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-15 17:34:06 +0000

    implement placeholder component

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,9 @@
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+import { initialTickets } from "@/data";
+import { ticketsPath } from "@/paths";
+import Link from "next/link";
+
 type TicketPageProps = {
   params: Promise<{
     ticketId: string;
@@ -7,7 +13,27 @@ type TicketPageProps = {
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
 
-  return <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
+  if (!ticket) {
+    return (
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath()}>Go to Tickets</Link>
+          </Button>
+        }
+      />
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
