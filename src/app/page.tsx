80810fbb9cfc440d54ebf43c3043550e4d501f8c
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-15 16:03:39 +0000

    add styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+import { ticketsPath } from "@/paths";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">HomePage</h2>
-      
-      <Link href="/tickets" className="text-sm underline">
-        Got to Tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-lg">HomePage</h2>
+
+        <Link href="/tickets" className="text-sm underline">
+          Got to Tickets
+        </Link>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
         <Link href={ticketsPath()} className="text-sm underline">
           Go to Tickets
         </Link>
+      </div>
     </div>
   );
 };
