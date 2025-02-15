a92846945e8dfb76e557e3e02e855cb7c3690772
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-15 19:51:37 +0000

    use ticket item component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,7 @@
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 
 const TICKET_ICONS = {
   OPEN: <LucideFileText />,
@@ -25,24 +16,7 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <span className="truncate">{ticket.title}</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <span className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </span>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
