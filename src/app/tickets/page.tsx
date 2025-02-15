994b5244984237b966e3606c54238828c71665b9
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-15 16:54:46 +0000

    replace ticket svg icons with library icons

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,54 +1,20 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
+import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
 import Link from "next/link";
 
-const CheckIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="size-6"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="m4.5 12.75 6 6 9-13.5"
-    />
-  </svg>
-);
-
-const DocumentIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    className="size-6"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-    />
-  </svg>
-);
-
-const PencilIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-</svg>
-
-)
-
 const TICKET_ICONS = {
-  OPEN: <DocumentIcon />,
-  IN_PROGRESS: <PencilIcon />,
-  DONE: <CheckIcon />,
+  OPEN: <LucideFileText />,
+  IN_PROGRESS: <LucideCheckCircle />,
+  DONE: <LucidePencil />,
 };
 
 const TicketsPage = () => {
@@ -65,10 +31,7 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <Card
-            key={ticket.id}
-            className="w-full max-w-[420px]"
-          >
+          <Card key={ticket.id} className="w-full max-w-[420px]">
             <CardHeader>
               <CardTitle className="flex gap-x-2">
                 <span>{TICKET_ICONS[ticket.status]}</span>
@@ -76,9 +39,7 @@ const TicketsPage = () => {
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <span
-                className="line-clamp-3 whitespace-break-spaces"
-              >
+              <span className="line-clamp-3 whitespace-break-spaces">
                 {ticket.content}
               </span>
             </CardContent>
