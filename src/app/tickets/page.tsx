cee69351273fc0ac8ea192171152bc08c48a937e
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-16 00:32:29 +0000

    convert to client component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { Heading } from "@/components/heading";
 import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { Ticket } from "@/features/ticket/types";
 import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const TICKET_ICONS = {
   OPEN: <LucideFileText />,
@@ -10,12 +15,24 @@ const TICKET_ICONS = {
 };
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const result = await getTickets();
+
+      setTickets(result);
+    };
+
+    fetchTickets();
+  });
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
