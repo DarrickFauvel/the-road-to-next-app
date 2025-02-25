21c113b13e9e2a01de8f2a05203667507cbb6dd6
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-25 13:57:47 +0000

    remove unused component import

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,10 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { TICKET_ICONS } from "../constants"
 import Link from "next/link"
 import { ticketPath } from "@/paths"
