b80ece97fd2e628dd6e6e1090c8b1abf8c53b77e
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-26 14:25:25 +0000

    remove unneeded getTickets call because of TicketList component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,9 @@
-import { Heading } from "@/components/heading";
-import { Spinner } from "@/components/spinner";
-import { TicketList } from "@/features/ticket/components/ticket-list";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
-import { Suspense } from "react";
+import { Heading } from "@/components/heading"
+import { Spinner } from "@/components/spinner"
+import { TicketList } from "@/features/ticket/components/ticket-list"
+import { Suspense } from "react"
 
 const TicketsPage = async () => {
-  const tickets = await getTickets();
-
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
@@ -15,7 +12,7 @@ const TicketsPage = async () => {
         <TicketList />
       </Suspense>
     </div>
-  );
-};
+  )
+}
 
-export default TicketsPage;
+export default TicketsPage
